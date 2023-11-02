ca32c77bd556fc631d7ec8dbbf5332d34de12a81
ahonkazi <aohinuzzaman420@gmail.com>
2023-11-02 14:00:25 +0600

    product header responsive

--- src/pages/product-page/components/header/ProductPageHeader.jsx
+++ src/pages/product-page/components/header/ProductPageHeader.jsx
@@ -6,11 +6,11 @@ import { ButtonAdd, ButtonDelete } from '../../../../shared/components/buttons/B
 
 const ProductPageHeader = () => {
     return (
-        <section className='section-style  flex justify-between items-center'>
+        <section className='section-style flex-col flex gap-2 xs:flex-row xs:items-center xs:justify-between '>
             <div className="">
                 <Button variant="outlined" style={{ borderColor: '#6B6B6B', color: '#6B6B6B' }}>Export</Button>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center justify-between gap-4">
                 <ButtonDelete />
 
                 <ButtonAdd bg={'#034B47'}>
