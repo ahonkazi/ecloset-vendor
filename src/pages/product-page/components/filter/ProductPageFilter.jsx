d3c6ef07b3a5919a7f4e808e226c62736126a202
ahonkazi <aohinuzzaman420@gmail.com>
2023-11-02 13:57:20 +0600

    filter responsive done

--- src/pages/product-page/components/filter/ProductPageFilter.jsx
+++ src/pages/product-page/components/filter/ProductPageFilter.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { SelectField, TextField } from '../../../../shared/components/input-fields/SelectField'
 import { Button } from '@mui/material'
-
+import { GetGlobalContext } from '../../../../context/GlobalContext'
 const ProductPageFilter = () => {
+    const globalContext = useContext(GetGlobalContext)
     return (
         <section className='section-style mt-4'>
-            <div className="grid grid-cols-4 gap-4">
+            <div className={`grid grid-cols-1  gap-4 ${globalContext.sidebarStatus ? 'lg:grid-cols-3 xl:grid-cols-4' : ' md:grid-cols-3 lg:grid-cols-4'}`}>
                 <div className="search">
                     <TextField placeholder={'Search Product'} variant={'filled'} />
                 </div>
@@ -15,7 +16,7 @@ const ProductPageFilter = () => {
                 <div className="price">
                     <TextField placeholder={'Max price'} variant={'filled'} />
                 </div>
-                <div className="actions grid grid-cols-2 gap-4">
+                <div className={`actions grid grid-cols-2 gap-4 ${globalContext.sidebarStatus ? 'lg:col-start-3 xl:col-start-auto' : 'md:col-start-3 lg:col-start-auto'}`}>
                     <button className='bg-LightBlue px-2 py-2 rounded text-white uppercase font-medium'>Filter</button>
                     <button className='bg-WhiteShade px-2 py-2 rounded text-Green uppercase font-medium'>Reset</button>
                 </div>
